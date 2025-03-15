e7a97b3877260858e8d3303ad78d86d01491238a
Bob <bob.vaz@hotmail.fr>
2025-03-15 14:04:40 +0100

    tailwind init

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* Rendu de l'application */}
+    <App />
   </React.StrictMode>
 );
